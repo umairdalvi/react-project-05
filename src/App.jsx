c0688bcb0c36c344f5b353d309c2e10476f24edd
umairdalvi <dev.ud02@proton.me>
2025-12-05 13:27:44 +0530

    import HeroSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/custom/Header";
 import "./App.css";
+import HeroSection from "./components/custom/HeroSection";
 
 const App = () => {
-  return (
-    <div>
-      <Header />
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <HeroSection />
+        </div>
+    );
 };
 
 export default App;
