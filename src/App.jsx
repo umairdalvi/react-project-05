7506af0a6c525feac6527d93751ab006d80cb0b2
umairdalvi <dev.ud02@proton.me>
2025-12-07 15:24:33 +0530

    import SectionTwo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/custom/Header";
 import "./App.css";
 import HeroSection from "./components/custom/HeroSection";
+import SectionTwo from "./components/custom/SectionTwo";
 
 const App = () => {
     return (
-        <div>
+        <div className="overflow-hidden">
             <Header />
             <HeroSection />
+            <SectionTwo />
         </div>
     );
 };
