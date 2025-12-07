db1c71005633c9c5c3444fa1ccb300b6fdd6f8be
umairdalvi <dev.ud02@proton.me>
2025-12-07 15:24:07 +0530

    create SectionTwo

---
+++ src/components/custom/SectionTwo.jsx
@@ -0,0 +1,33 @@
+const SectionTwo = () => {
+    return (
+        <div className="container">
+            <div className="section-two">
+                <div className="section-two-banner">
+                    <img src="./images/img_05.jpg" alt="section-two-banner" />
+                    <img
+                        className="section-two-deco section-two-deco-01"
+                        src="./images/shape_28.svg"
+                        alt="shape_28"
+                    />
+                    <img
+                        className="section-two-deco section-two-deco-02"
+                        src="./images/shape_29.svg"
+                        alt="shape_29"
+                    />
+                    <img
+                        className="section-two-deco section-two-deco-03"
+                        src="./images/shape_30.svg"
+                        alt="shape_30"
+                    />
+                    <img
+                        className="section-two-deco section-two-deco-04"
+                        src="./images/shape_31.svg"
+                        alt="shape_31"
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SectionTwo;
