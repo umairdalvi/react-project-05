b70146dd6fd64259426c33b8c1172764cab82f33
umairdalvi <dev.ud02@proton.me>
2025-12-08 15:26:32 +0530

    create FeatureCard

---
+++ src/components/custom/FeatureCard.jsx
@@ -0,0 +1,22 @@
+const FeatureCard = ({ imgUrl, featureName, featureDsc }) => {
+    return (
+        <div className="feature-card">
+            <div className="feature-img-wrapper">
+                <img src={imgUrl} alt="" className="feature-img" />
+            </div>
+            <div className="feature-name">
+                <p>{featureName}</p>
+            </div>
+            <div className="feature-dsc">
+                <p>{featureDsc}</p>
+            </div>
+            <div className="feature-cta">
+                <a href="#">
+                    <i className="ri-arrow-right-long-line"></i>
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default FeatureCard;
